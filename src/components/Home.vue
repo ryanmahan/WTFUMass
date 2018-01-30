1df8ccd4d2eb8af0669adf1125671b1a423e1611
ryanmahan <ryanmahan97@gmail.com>
2018-01-30 13:45:02 -0500

    Added sorting by vote amount to list

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,19 @@
 // eslint-disable-next-line
 <template>
   <div id="home">
-    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
     <v-app id="cardslist">
         <h1 id='header'> What to fix? Submit a fix or vote on some here! </h1>
-        <v-layout v-for="project in projects" v-bind:key='project.votes'>
+        <v-layout v-for="project in sortedByVote" v-bind:key='project._id'>
           <v-flex id='layout'>
               <v-card id='card' data-id=project.id>
+                
                 <v-card-title primary-title>
                   <div>
                     <h3 class="headline mb-0">{{ project.title }}</h3>
                     <div class='title left'>{{ project.votes }} Votes</div>
                   </div>
                 </v-card-title>
+
                 <v-card-text>
                   {{ project.description }}
                 </v-card-text>
@@ -20,6 +21,7 @@
                 <v-card-actions>
                   <v-btn flat @click.native='voteUp(project)' id='votebutton'>Vote for this</v-btn>
                 </v-card-actions>
+
               </v-card>
           </v-flex>
         </v-layout>
@@ -70,6 +72,20 @@ export default {
       .catch(function (err) {
         handleError(err)
       })
+    }
+  },
+  computed: {
+    sortedByVote: function() {
+      function compare(a, b) {
+        if (a.votes > b.votes) {
+          return -1;
+        }
+        if (a.votes < b.votes) {
+          return 1;
+        }
+        return 0;
+      }
+      return this.projects.sort(compare)
     }
   },
   created: function() {

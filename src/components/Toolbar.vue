2cfda315ef4a4fd06d063decbf71537cd0bf5e28
ryanmahan <ryanmahan97@gmail.com>
2018-02-12 11:36:15 -0500

    Fixed toolbar for smaller screens

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,11 +1,35 @@
 <template>
   <v-app>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
   <v-toolbar id="bar" dark>
     <v-toolbar-title id="title"> 
       WTF UMass
     </v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-toolbar-items>
+    <!-- for smaller screens -->
+    <v-menu class="hidden-md-and-up" offset-y>
+      <v-btn slot='activator' icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+      <v-list offset-y>
+        <v-list-tile @click='pushCreate' flat>Submit a fix</v-list-tile>
+        <v-list-tile @click='pushHome' flat>Vote on fixes</v-list-tile>
+        <v-list-tile v-if='!loggedIn' @click='pushLogin' flat>{{name}}</v-list-tile>
+        <div v-if='loggedIn'>
+            <v-list-tile @click='logout()'>
+              <v-list-tile-title>Log Out</v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile @click='pushSettings()'>
+              <v-list-tile-title>Settings</v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile @click='pushAbout()'>
+              <v-list-tile-title>About</v-list-tile-title>
+            </v-list-tile>
+        </div>
+      </v-list>
+    </v-menu>
+    <!-- For larger screens -->
+    <v-toolbar-items class="hidden-sm-and-down">
       <v-btn @click='pushCreate' flat>Submit a fix</v-btn>
       <v-btn @click='pushHome' flat>Vote on fixes</v-btn>
       <v-btn v-if='!loggedIn' @click='pushLogin' flat>{{name}}</v-btn>
@@ -88,7 +112,7 @@ export default {
       this.user = user
       this.name = user.fname
     } else {
-      this.name = 'login'
+      this.name = 'Log in'
     }
   }
 }

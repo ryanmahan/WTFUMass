3af990a0a8a3ff48abb5e7d0ec67ccb7badb5ff8
ryanmahan <ryanmahan97@gmail.com>
2018-03-07 16:20:43 -0500

    Fix some frontend issues with google sign in

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -59,16 +59,19 @@ router.post('/verify', function(req, res) {
   const {OAuth2Client} = require('google-auth-library');
   const client = new OAuth2Client(CLIENT_ID);
   let token = req.body.token
-  console.log('running verify')
   async function verify() {
     const ticket = await client.verifyIdToken({
         idToken: token,
         audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
         // Or, if multiple clients access the backend:
         //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-  });
-    let payload = ticket.getPayload();
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
   }
+verify().catch(console.error);
   const userid = payload['sub'];
   console.log(payload)
 

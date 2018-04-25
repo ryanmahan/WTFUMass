a5b403829686727e645ec469b0ce44e748336762
ryanmahan <ryanmahan97@gmail.com>
2018-04-25 11:58:24 -0400

    More bugs?

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -63,10 +63,6 @@ export default {
           if (res.data.success) {
           self.$cookie.set('user', JSON.stringify(res.data.doc), "0")
           self.$bus.$emit('name', res.data.doc)
-          location.reload()
-          self.$router.push({
-            name: 'Home'
-          })
         } else {
           self.error = true
         }
